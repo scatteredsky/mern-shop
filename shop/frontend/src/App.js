eb89cd021d891800b1328446dff3af44cf170530
scatteredsky <67299222+scatteredsky@users.noreply.github.com>
2021-07-07 22:04:13 -0500

    latest changes

--- shop/frontend/src/App.js
+++ shop/frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to Pro Shop</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Pro Shop</h1>
+        </Container>  
+      </main>
+      <Footer />
       
     </div>
   );
